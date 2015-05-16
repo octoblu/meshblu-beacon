b42019a9798b60c9eb2c22fb5a761451db85d4d5
virgilvox <moheeb.zara@citrix.com>
2015-05-16 14:44:48 -0700

    added options

--- index.js
+++ index.js
@@ -11,23 +11,27 @@ var prevACC;
 var MESSAGE_SCHEMA = {
   type: 'object',
   properties: {
-    exampleBoolean: {
-      type: 'boolean',
-      required: true
-    },
-    exampleString: {
-      type: 'string',
-      required: true
-    }
+   
   }
 };
 
 var OPTIONS_SCHEMA = {
   type: 'object',
   properties: {
-    firstExampleOption: {
+    uuid: {
+      title: "UUID: Leave blank to scan for ANY nearby beacon",
       type: 'string',
-      required: true
+      required: false
+    },
+    majorId: {
+      title: "Major Id: Optional",
+      type: 'number',
+      required: false
+    },
+    minorId: {
+      title: "Minor Id: Optional",
+      type: 'number',
+      required: false
     }
   }
 };
@@ -49,12 +53,31 @@ Plugin.prototype.onConfig = function(device){
   var self = this;
   this.setOptions(device.options||{});
 
-  Bleacon.startScanning();
+if (device.options.uuid && device.options.majorId && device.options.minorId){
+
+      Bleacon.startScanning(device.options.uuid, device.options.majorId, device.options.minorId);
+
+}else if(device.options.uuid && device.options.majorId){
+
+      Bleacon.startScanning(device.options.uuid, device.options.majorId);
+
+
+}else if(device.options.uuid){
+
+      Bleacon.startScanning(device.options.uuid);
+
+
+}else{
+
+       Bleacon.startScanning();
+
+}
+
 
   Bleacon.on('discover', function(bleacon) {
+   
 
     setTimeout(function() {
-     
 
 
   if(bleacon.rssi != prevRSSI && bleacon.accuracy != prevACC){
@@ -66,7 +89,8 @@ Plugin.prototype.onConfig = function(device){
       prevRSSI = bleacon.rssi;
       prevACC = bleacon.accuracy;
 
-}, 200); 
+}, 200);  //end timeout
+
 
     
 });
